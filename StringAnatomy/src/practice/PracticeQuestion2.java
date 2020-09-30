aad2796f7176fcec57393809fc5d57a80a0f13d1
Abheek <rabheek@gmail.com>
2020-09-29 23:51:24 -0400

    Solved PracticeQuestion2

--- StringAnatomy/src/practice/PracticeQuestion2.java
+++ StringAnatomy/src/practice/PracticeQuestion2.java
@@ -1,10 +1,19 @@
 package practice;
 
+/**
+ * PracticeQuestion2 tests trim() knowledge along with String immutable properties.
+ * 
+ * @author abheek.srivastava
+ *
+ */
 public class PracticeQuestion2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String str = " ";
+		
+		str.trim(); // there will be no change on str since String objects are immutable
+		
+		System.out.println(str.equals("")+" "+str.isEmpty()); // false false
 	}
 
 }
