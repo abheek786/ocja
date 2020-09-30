16f47c1cb6b9e589c22cc00f76df8bcf4ec79bf8
Abheek <rabheek@gmail.com>
2020-09-29 23:35:13 -0400

    Solving a String problem

--- StringAnatomy/src/practice/StringTest.java
+++ StringAnatomy/src/practice/StringTest.java
@@ -4,6 +4,18 @@ public class StringTest {
 
 	public static void main(String[] args) {
 		String ta = "A";
+		
+		ta = ta.concat("B");  // AB
+		
+		String tb = "C";
+		
+		ta = ta.concat(tb); // ABC
+		
+		ta.replace('C', 'D');
+		
+		ta = ta.concat(tb); // ABCC
+		
+		System.out.println(ta);
 	}
 
 }
